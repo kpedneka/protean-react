b8dad6f573df13ad1fd320ae4063620235f69226
RooshhilPatel <rushil.patel14@gmail.com>
2017-06-29 17:30:06 -0400

    added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Navigation from './Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
